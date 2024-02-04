a574490de56122851fc1dff647b4105385bd20f4
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-02-04 23:30:16 +0000

    Sun, Feb 4, 2024, 3:30 PM -08:00

--- calculator.c
+++ calculator.c
@@ -1,18 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int add(void);
+int add(int a, int b);
 
 int main(void)
 {
     int x = get_int("x: ");
     int y = get_int("y: ");
 
-    int z = add();
+    int z = add(x, y);
     printf("%i\n", z);
 }
 
-int add(void)
+int add(int a, int b)
 {
-    return x + y;
+    return a + b;
 }
