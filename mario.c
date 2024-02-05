a8b58146b50cd1e4000eb116c1c592fdcc83bac2
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-02-05 04:01:49 +0000

    Sun, Feb 4, 2024, 8:01 PM -08:00

--- mario.c
+++ mario.c
@@ -6,8 +6,9 @@ int main (void)
     const int n = get_int("Size: ");
     while (n < 1)
     {
-        n=get_int("Size: ");
+        n = get_int("Size: ");
     }
+
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j< n; j++)
