3c3d676eac0759451ef096e2febb6a328e0315ea
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-02-05 03:57:59 +0000

    Sun, Feb 4, 2024, 7:57 PM -08:00

--- mario.c
+++ mario.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main (void)
 {
-    int n = 3;
+    const int n = get_int("Size: ");
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j< n; j++)
         {
-            printf("#";
+            printf("#");
         }
         printf("\n");
     }
