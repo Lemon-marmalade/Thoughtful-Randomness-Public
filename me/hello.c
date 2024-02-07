f0b03db81521643ebdbeef5df87b3ba8bb13c590
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-02-07 04:16:04 +0000

    Tue, Feb 6, 2024, 8:16 PM -08:00

--- me/hello.c
+++ me/hello.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 int main(void)
 {
-    string name = get_string("What art thou name? ");
+    string answer = get_string("What art thou name? ");
 
-printf("Hello, %s\n" , name);
+    printf("Hello, %s\n" , answer);
 }
