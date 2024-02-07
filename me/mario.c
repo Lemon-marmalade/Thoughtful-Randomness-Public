8784d2d7270d657bafc9722b720ffe25af0dd8ea
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-02-07 16:46:06 +0000

    Wed, Feb 7, 2024, 8:46 AM -08:00

--- me/mario.c
+++ me/mario.c
@@ -4,13 +4,14 @@
 int main (void)
 {
     int n;
+// rejects answers less than 1 or greater than 8
     do
     {
         n = get_int("Height: ");
     }
-    while (n<1);
-    
-    for (int i = 0; i < n; i++)
+    while (n < 1 && n > 8);
+// r is for # of rows
+    for (int r = 0; r < n; r++)
     {
         for (int j = 0; j< n; j++)
         {
