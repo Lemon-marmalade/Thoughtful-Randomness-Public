49121c27d35980b4f7084a584a6ee0c30209cf59
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-02-08 02:43:33 +0000

    Wed, Feb 7, 2024, 6:43 PM -08:00

--- me/mario.c
+++ me/mario.c
@@ -9,7 +9,7 @@ int main (void)
     {
         n = get_int("Height: ");
     }
-    while (n < 1 || n > 8);
+    while (n < 1 && n > 8);
 // r is for # of rows
     for (int r = 0; r < n; r++)
     {
