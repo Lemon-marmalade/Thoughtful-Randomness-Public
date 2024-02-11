78c2c83b860bfb1f6bfa1c49e12f0e24fcc7a922
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-02-11 18:40:44 +0000

    Sun, Feb 11, 2024, 10:40 AM -08:00

--- cash/cash.c
+++ cash/cash.c
@@ -4,30 +4,4 @@
 int main(void)
 
 {
-    int x;
-    do
-    {
-         x = get_int("Change owed: ");
-    }
-    while (x < 1 );
-     // write conditions
-     if (x > 25)
-     {
-          int a = (x / 25);
-          int b = (x % 25);
-
-          if (b > 10)
-          {
-               int c = (b / 10)
-               int d = (b % 10)
-          }
-          else if (b > 5 && b < 10)
-          {
-               int e = (b / 5)
-               int f = (b % 5)
-          }
-          else if (b )
-
-          printf("Minimum amount of coins: %i \n", x / 25);
-     }
-}
+    int input = get_int("Change owed: ")
