1d3560ab2f94f1408dbc33296fd8120cb1278090
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-02-11 18:52:08 +0000

    Sun, Feb 11, 2024, 10:52 AM -08:00

--- cash/cash.c
+++ cash/cash.c
@@ -4,5 +4,24 @@
 int main(void)
 
 {
-    int input = get_int("Change owed: ")
-    int coins[4]= {25, 10, 5, 1}
+     //prompt user for change owed, in cents
+     do
+     {
+    int cents = get_int("Change owed: ");
+     }
+     while (cents < 1);
+}
+    // calculate how many quarters needed
+    // sutract value of quarters from total change owed
+
+    // calculate how many dimes needed for previous difference
+    // subtract value of dimes from remaining cents
+
+    // calculate how many nickels needed for previous difference
+    // subtract value of nickels from remaining cents
+
+    // calculate how many pennies needed
+    // subtract value of pennies from remaining cents
+
+    // add the number of quarters, dimes, nickels, and pennies
+    // print the sum
