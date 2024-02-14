fdc821556d2c1f3f2210d5431d6f2a3e1704479e
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-02-14 01:18:18 +0000

    Tue, Feb 13, 2024, 5:18 PM -08:00

--- credit/credit.c
+++ credit/credit.c
@@ -19,20 +19,20 @@ int main(void)
     {
         if((number > 340000000000000 && number < 350000000000000)||(number > 370000000000000 && number < 380000000000000))
         {
-            printf("AMEX \n");
+            printf("AMEX\n");
         }
         else if(number > 5100000000000000 && number < 5600000000000000)
         {
-            printf("MASTERCARD \n");
+            printf("MASTERCARD\n");
         }
         else if((number > 4000000000000 && number < 5000000000000)||(number > 4000000000000000 && number < 5000000000000000))
         {
-            printf("AMEX \n");
+            printf("VISA\n");
         }
     }
     else
     {
-        printf("INVALID \n");
+        printf("INVALID\n");
     }
 }
 
