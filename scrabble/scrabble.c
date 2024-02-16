d3781fb78af42b895366eb548b4857f8fa0a3a32
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-02-16 16:39:02 +0000

    Fri, Feb 16, 2024, 8:39 AM -08:00

---
+++ scrabble/scrabble.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+
+}
+// prompt usuers for input
+// compute score of each word
+// print winner based on which score is higher
