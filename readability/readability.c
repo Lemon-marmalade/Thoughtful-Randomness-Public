bfb80eb788a3c1e2008d2761881f9c1852f4ecd8
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-02-19 22:43:23 +0000

    Mon, Feb 19, 2024, 2:43 PM -08:00

--- readability/readability.c
+++ readability/readability.c
@@ -16,7 +16,7 @@ int main(void)
     int words = calculate_words(text);
     int sentences = calculate_sentences(text);
 
-    int index = 0.0588 * (letters * 100/words) - 0.296 * (sentences * 10/words) - 15.8;
+    int index = 0.0588 * (letters * 100/words) - 0.296 * (sentences * 100/words) - 15.8;
     printf("%i\n", index);
 }
 // prompt user for text
