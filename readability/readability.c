78b0b1d8d749475d4f9189a157821f718d1695e2
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-02-19 22:31:52 +0000

    Mon, Feb 19, 2024, 2:31 PM -08:00

--- readability/readability.c
+++ readability/readability.c
@@ -52,7 +52,8 @@ int calculate_sentences(string text)
     int sentences = 0;
     for(int i = 0; i < strlen(text); i++)
     {
-        if(ispunct(text[i]))
+        char comma = ,
+        if(ispunct(text[i]) && text[i] != comma)
         {
             sentences = sentences + 1;
         }
