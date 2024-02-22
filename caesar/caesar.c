49e3745908355a07fadfcfdd3e653a6f788abc77
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-02-22 04:41:48 +0000

    Wed, Feb 21, 2024, 8:41 PM -08:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,12 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, string argv[])
 {
     // check that there is one command argument that is a number
     if (argc == 2)
     {
-        printf("%s\n", argv[1]);
+        int key = atoi(argv[1]);
+        printf("%i\n", key);
         // prompt for plaintext
         // apply the key
         //output ciptertext
