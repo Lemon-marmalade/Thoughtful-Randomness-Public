a9d89b7709b2a94642661d61ed9cb027d483acb7
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-02-22 04:48:12 +0000

    Wed, Feb 21, 2024, 8:48 PM -08:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,23 +1,24 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
     // check that there is one command argument that is a number
-    if (argc == 2)
+    if (argc != 2)
     {
-        int key = atoi(argv[1]);
-        printf("%i\n", key);
-        // prompt for plaintext
-        // apply the key
-        //output ciptertext
-    }
-    else
-    {
-
         printf("Usage: ./caesar key\n");
         return 1;
     }
+    for(int i = 0; i < strlen (arv[1]); i++)
+    {
+        
+    }
+
+        // prompt for plaintext
+        // apply the key
+        //output ciptertext
+
 }
 
