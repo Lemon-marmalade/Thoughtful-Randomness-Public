1db9010082728f35f747510b46b2e84847f115b5
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-14 23:21:26 +0000

    Thu, Mar 14, 2024, 4:21 PM -07:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -68,14 +68,14 @@ bool vote(string name)
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        if ( strcmp(candidates[i].name, name) == 0)
+        if (strcmp(candidates[i].name, name) == 0)
         {
             candidates[i].votes++;
             return true;
         }
     }
     //
-     return false;
+    return false;
 }
 
 // Print the winner (or winners) of the election
@@ -94,7 +94,7 @@ void print_winner(void)
     {
         if (max == candidates[i].votes)
         {
-        printf("%s\n", candidates[i].name);
+            printf("%s\n", candidates[i].name);
         }
     }
     //
