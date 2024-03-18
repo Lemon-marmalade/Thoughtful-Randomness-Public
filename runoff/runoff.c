ccc79fa2fb0db5195b3bb6f6536b1e73e249b338
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-18 01:29:41 +0000

    Sun, Mar 17, 2024, 6:29 PM -07:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -200,7 +200,7 @@ bool is_tie(int min)
     {
             if (candidates[a].votes == min)
             {
-                printf("tie!\n");
+                return true;
             }
     }
     //
