928acb38d718f63fbfcb3a1c99d74c0b738f2d6f
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-18 02:36:37 +0000

    Sun, Mar 17, 2024, 7:36 PM -07:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -181,13 +181,15 @@ bool print_winner(void)
 int find_min(void)
 {
     // TODO
-    int min = candidates[0].votes;
-    for (int i = 1; i < candidate_count; i++)
+    int min = MAX;
+    for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].eliminated == false)
-        [if (candidates[i].votes < min)
         {
-            min = candidates[i].votes;
+            if (candidates[i].votes < min)
+            {
+                min = candidates[i].votes;
+            }
         }
         return min;
     }
