a7a1ba0e2dc2a15abadc02ab74b5ba7fb8716e61
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-23 04:01:25 +0000

    Fri, Mar 22, 2024, 9:01 PM -07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -135,6 +135,15 @@ void record_preferences(int ranks[])
 void add_pairs(void)
 {
     // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            if (preferences[i][j] != 0)
+            {
+                
+            }
+    //
     return;
 }
 
