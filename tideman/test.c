b7689156c6e9c85cfc7aabc847eb4767f648af77
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-24 18:23:46 +0000

    Sun, Mar 24, 2024, 11:23 AM -07:00

--- tideman/test.c
+++ tideman/test.c
@@ -6,5 +6,17 @@ int main(void)
     int x = 0;
     int y = 1;
     x = y;
+    for (int i = 0; i < 3; i++)
+        {
+            if (locked[loser][loser_loser] == true)
+            {
+                loser = loser_loser;
+                if (loser == winner)
+                {
+                cycle = true;
+                break;
+                }
+            }
+        }
     printf("%i,%i",x,y);
 }
