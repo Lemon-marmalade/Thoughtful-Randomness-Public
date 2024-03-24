9bda138b979eafecadfaca293fa16a1a77a97f6f
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-24 00:37:35 +0000

    Sat, Mar 23, 2024, 5:37 PM -07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -161,7 +161,13 @@ void add_pairs(void)
 void sort_pairs(void)
 {
     // TODO
-    
+    for (int i = 0; i < pair_count; i++)
+    {
+        for (int j = i + 1; j < pair_count; j++)
+        {
+            if 
+        }
+    }
     //
     return;
 }
