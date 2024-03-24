a9562e6b43b7bf34d041298603a94f6cddb65452
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-24 15:30:37 +0000

    Sun, Mar 24, 2024, 8:30 AM -07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -164,10 +164,9 @@ void sort_pairs(void)
         int swapped = 0;
         for (int i = 0; i < pair_count - 1; i++)
         {
-            int strength = preferences[pairs[i].winner][pairs[i].loser] - preferences[pairs[j].winner][pairs[j].loser]
             for (int j = i + 1; j < pair_count; j++)
             {
-                if (pairs[i].winner - pairs[i].loser < pairs[j].winner)
+                if (preferences[pairs[i].winner][pairs[i].loser] < preferences[pairs[j].winner][pairs[j].loser])
                 {
                     pair tmp = pairs [i];
                     pairs[i] = pairs [j];
