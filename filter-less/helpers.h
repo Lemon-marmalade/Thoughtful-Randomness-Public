687a6ca37ca2ccea6855aeba7346f1b2a4e9ed69
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-26 03:44:21 +0000

    Mon, Mar 25, 2024, 8:44 PM -07:00

--- filter-less/helpers.h
+++ filter-less/helpers.h
@@ -1,4 +1,5 @@
 #include "bmp.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width]);
@@ -8,7 +9,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width]);
     {
         for (int j = 0; j < width; j++)
         //take average of hexadecimal value, and update pixel values to that average
-        int average = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue)/3;
+        int average = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue)/3.0);
         image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = average;
 
     }
