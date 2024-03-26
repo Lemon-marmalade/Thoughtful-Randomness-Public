ffab9e91179b31fb3b31ff66241b7f834286e3f0
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-26 03:49:34 +0000

    Mon, Mar 25, 2024, 8:49 PM -07:00

--- filter-less/helpers.h
+++ filter-less/helpers.h
@@ -17,6 +17,23 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width]);
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width]);
+{
+    // loop over each pixels
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // compute sepia values, and update those values
+            int originalRed = image[i][j].rgbtRed;
+            int originalGreen = image[i][j].rgbtGreen;
+            int originalBlue = image[i][j].rgbtBlue;
+
+            int sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue
+            int sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue
+            int sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue
+        }
+    }
+}
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width]);
