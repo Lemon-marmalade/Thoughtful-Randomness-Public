2f046eed6b9821ad074d598053b59a8b674f0307
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-26 04:24:38 +0000

    Mon, Mar 25, 2024, 9:24 PM -07:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,14 +1,51 @@
 #include "helpers.h"
+#include <math.h>
+
+// added to help sepia
+int fit_limit(int sepia);
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    //loop over each pixelt
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            //take average of hexadecimal value, and update pixel values to that average
+            int average = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue)/3.0);
+            image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = average;
+        }
+    }
     return;
 }
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    // loop over each pixels
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // compute sepia values, and update those values
+            int originalRed = image[i][j].rgbtRed;
+            int originalGreen = image[i][j].rgbtGreen;
+            int originalBlue = image[i][j].rgbtBlue;
+
+            int sepiaRed = round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue);
+            int sepiaGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
+            int sepiaBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
+
+            fit_limit(sepiaRed);
+            fit_limit(sepiaGreen);
+            fit_limit(sepiaBlue);
+
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
+        }
+    }
     return;
 }
 
@@ -22,4 +59,13 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     return;
+}
+
+int fit_limit(int sepia)
+{
+    if (sepia > 255)
+    {
+        sepia = 255;
+    }
+    return sepia;
 }
--- filter-less/helpers.h
+++ filter-less/helpers.h
@@ -1,50 +1,11 @@
 #include "bmp.h"
-#include <math.h>
-
-// added to help sepia
-int fit_limit(int sepia);
 
 // Convert image to grayscale
-void grayscale(int height, int width, RGBTRIPLE image[height][width])
-{
-    //loop over each pixelt
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            //take average of hexadecimal value, and update pixel values to that average
-            int average = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue)/3.0);
-            image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = average;
-        }
-    }
-}
+void grayscale(int height, int width, RGBTRIPLE image[height][width]);
+
 // Convert image to sepia
-void sepia(int height, int width, RGBTRIPLE image[height][width])
-{
-    // loop over each pixels
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            // compute sepia values, and update those values
-            int originalRed = image[i][j].rgbtRed;
-            int originalGreen = image[i][j].rgbtGreen;
-            int originalBlue = image[i][j].rgbtBlue;
+void sepia(int height, int width, RGBTRIPLE image[height][width]);
 
-            int sepiaRed = round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue);
-            int sepiaGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
-            int sepiaBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
-
-            fit_limit(sepiaRed);
-            fit_limit(sepiaGreen);
-            fit_limit(sepiaBlue);
-
-            image[i][j].rgbtRed = sepiaRed;
-            image[i][j].rgbtGreen = sepiaGreen;
-            image[i][j].rgbtBlue = sepiaBlue;
-        }
-    }
-}
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width]);
 
@@ -52,11 +13,3 @@ void reflect(int height, int width, RGBTRIPLE image[height][width]);
 void blur(int height, int width, RGBTRIPLE image[height][width]);
 
 
-int fit_limit(int sepia)
-{
-    if (sepia > 255)
-    {
-        sepia = 255;
-    }
-    return sepia;
-}
