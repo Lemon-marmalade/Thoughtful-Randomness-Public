388348bb8d230b0cc017d3a4de90a07207001571
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-26 03:37:51 +0000

    Mon, Mar 25, 2024, 8:37 PM -07:00

--- filter-less/helpers.h
+++ filter-less/helpers.h
@@ -2,6 +2,15 @@
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width]);
+{
+    //loop over each pixelt
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        //take average of hexadecimal value, and update pixel values to that average
+        
+    }
+}
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width]);
