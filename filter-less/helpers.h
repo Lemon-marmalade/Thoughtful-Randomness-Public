69155681fa5d8263a46219ed28beae2c174927bb
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-26 03:50:53 +0000

    Mon, Mar 25, 2024, 8:50 PM -07:00

--- filter-less/helpers.h
+++ filter-less/helpers.h
@@ -28,9 +28,10 @@ void sepia(int height, int width, RGBTRIPLE image[height][width]);
             int originalGreen = image[i][j].rgbtGreen;
             int originalBlue = image[i][j].rgbtBlue;
 
-            int sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue
-            int sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue
-            int sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue
+            int sepiaRed = round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue)
+            int sepiaGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue)
+            int sepiaBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue)
+            
         }
     }
 }
