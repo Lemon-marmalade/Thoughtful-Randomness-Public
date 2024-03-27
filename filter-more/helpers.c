dcec84c1ed2be7c773d8c73ba7d3db89f6532042
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-27 03:44:45 +0000

    Tue, Mar 26, 2024, 8:44 PM -07:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -119,6 +119,10 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                         GyGreen = GyGreen + copy[a][b].rgbtGreen * Gy[a][b];
                         GyBlue = GyBlue + copy[a][b].rgbtBlue * Gy[a][b];
                     }
+                    else
+                    {
+                        continue;
+                    }
                 }
                 // calculate square root of Gx^2+Gy^2
                 int SobelRed = round(sqrt(pow(GxRed,2)+pow(GyRed,2)));
