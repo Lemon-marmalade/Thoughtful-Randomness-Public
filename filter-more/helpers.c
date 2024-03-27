0eba9fbec91d537535567fe829ea168d18252984
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-27 04:21:45 +0000

    Tue, Mar 26, 2024, 9:21 PM -07:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -130,6 +130,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
 
             // deal with values exceeding 255
             int SobelRBG[3];
+            
             SobelRBG[0] = round(sqrt(pow(SobelRed, 2) + pow(SobelRed, 2)));
             SobelRBG[1] = round(sqrt(pow(SobelGreen, 2) + pow(SobelGreen, 2)));
             SobelRBG[2] = round(sqrt(pow(SobelBlue, 2) + pow(SobelBlue, 2)));
