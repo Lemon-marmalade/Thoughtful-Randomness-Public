49cd2ba9c4d9df27e17cb2a0f42413f1e30af16c
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-27 19:35:26 +0000

    Wed, Mar 27, 2024, 12:35 PM -07:00

--- filter-more/test.c
+++ filter-more/test.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <math.h>
 
-double x = 0.14;
-int y = 3;
+int main (void)
+{
+    double x = 1.5;
 
-int square = round(pow(x,y));
+int square = round(pow(x,2));
 printf("%i\n", square);
+}
