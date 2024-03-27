abfce35a3a352a73e8b9cac0da64ed37bb594cd3
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-27 03:42:22 +0000

    Tue, Mar 26, 2024, 8:42 PM -07:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -10,8 +10,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             // take average of hexadecimal value, and update pixel values to that average
-            int average =
-                round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
+            int average = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
             image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = average;
         }
     }
