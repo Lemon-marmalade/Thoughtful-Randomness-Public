2beb8926e8fa6026ec0eebc5102fb6851084d2c8
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-27 18:47:02 +0000

    Wed, Mar 27, 2024, 11:47 AM -07:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -111,7 +111,11 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                     int c = i - 1 + a;
                     int d = j - 1 + b;
                     // if pixel exceeds boundaries, disregard pixels that are outside image
-                    if (c >= 0 && c < height && d >= 0 && d < width)
+                    if (c < 0 || c > (height - 1) || d < 0 || d >= (width - 1))
+                    {
+                        continue;
+                    }
+                    else
                     {
                         GxRed += copy[c][d].rgbtRed * Gx[c][d];
                         GxGreen += copy[c][d].rgbtGreen * Gx[c][d];
