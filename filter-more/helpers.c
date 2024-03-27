f5fca6fae3ff48f34c7eb3b7d35aa4a67ce05247
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-27 01:35:38 +0000

    Tue, Mar 26, 2024, 6:35 PM -07:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -90,6 +90,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
         }
     }
     int Gx[3][3] = {{-1,0,1},{-2,0,2},{-1,0,1}}
+    int Gy[3][3] = {{-1,-2,-1},{-0,0,0},{1,2,1}}
 
     // loop over all pixels
      for (int i = 0; i < height; i++)
