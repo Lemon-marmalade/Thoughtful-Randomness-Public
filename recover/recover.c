37ebe398484a9174922b6b9c9cfde3c2dccf11e7
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-28 03:16:57 +0000

    Wed, Mar 27, 2024, 8:16 PM -07:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,6 +3,16 @@
 
 int main(int argc, char *argv[])
 {
- // accept only one command line argument that has to be the name of a forensic image
- //
+    // accept only one command line argument that has to be a file name
+    if (argc != 2)
+    {
+        printf{"Usage: ./recover FILE\n"};
+        return 1;
+    }
+    // open memory card to read
+    FILE *card = fopen(argv[1], "r")
+    if (card == NULL)
+    {
+        printf{"Could not open %s\n", input};
+    }
 }
