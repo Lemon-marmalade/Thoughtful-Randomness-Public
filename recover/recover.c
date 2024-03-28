73ad62e644243cf5031b4ee4a17e3d44aa3593dc
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-28 21:43:55 +0000

    Thu, Mar 28, 2024, 2:43 PM -07:00

--- recover/recover.c
+++ recover/recover.c
@@ -20,7 +20,9 @@ int main(int argc, char *argv[])
     // Create buffer for a block of data, and counter for # of images found
     uint8_t buffer[512];
     int image_count =0;
-    char filename[8];
+    // array of chars with 3 digits, one period, 'jpg' and nul character
+    char jpgname[8];
+    FILE *image = NULL;
     // While there is still data left
     while (fread(buffer, 1, sizeof(buffer), card) == sizeof(buffer))
     {
@@ -30,7 +32,9 @@ int main(int argc, char *argv[])
             // if buffer i to i+2 is equal to the given values, and if the intersection of buffer[i+3] and 1111 0000 is 1110 0000 (meaning buffer[i+3] must have first four bits of 1110)
             if (buffer[i] == 0xff && buffer[i + 1] == 0xd8 && buffer[i + 2] == 0xff && (buffer[i + 3] & 0xf0) == 0xe0)
             {
-                sprintf(output,"%03i.jpg",image_count);
+                sprintf(jpgname,"%03i.jpg",image_count);
+                img = fopen(jpgname, "w");
+                fwrite(buffer, 1 ,sizeof(buffer), image)
             }
         }
     }
