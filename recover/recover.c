8e24e99355853acf491a10390650aadf68074a9d
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-28 22:25:44 +0000

    Thu, Mar 28, 2024, 3:25 PM -07:00

--- recover/recover.c
+++ recover/recover.c
@@ -45,5 +45,9 @@ int main(int argc, char *argv[])
                 fwrite(buffer, 1 ,sizeof(buffer), img);
             }
         }
+        else if (fopen(img) == true)
+        {
+            
+        }
     }
 }
