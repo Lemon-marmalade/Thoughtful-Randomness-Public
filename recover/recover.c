7a341365ba19490a7b2bff1430cd6a47ac26495f
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-28 22:08:20 +0000

    Thu, Mar 28, 2024, 3:08 PM -07:00

--- recover/recover.c
+++ recover/recover.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
     }
     // Create buffer for a block of data, and counter for # of images found
     uint8_t buffer[512];
-    int image_count = 0;
+    int file_count = 0;
     // array of chars with 3 digits, one period, 'jpg' and nul character
     char filename[8];
     FILE *img;
@@ -32,11 +32,18 @@ int main(int argc, char *argv[])
         {
             if (buffer[i] == 0xff && buffer[i + 1] == 0xd8 && buffer[i + 2] == 0xff && (buffer[i + 3] & 0xf0) == 0xe0)
             {
+                if (file_count == 0)
+                {
                 sprintf(filename,"%03i.jpg",image_count);
-                image_count++;
+                file_count++;
                 img = fopen(filename, "w");
                 fwrite(buffer, 1 ,sizeof(buffer), img);
+
+                }
+            else
+            {
                 fclose[img];
+            }
             }
         }
     }
