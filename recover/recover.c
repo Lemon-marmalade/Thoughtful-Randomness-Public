3e606812604b2d8413dbc3fc7d19875cc1f13f06
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-28 21:49:27 +0000

    Thu, Mar 28, 2024, 2:49 PM -07:00

--- recover/recover.c
+++ recover/recover.c
@@ -29,7 +29,6 @@ int main(int argc, char *argv[])
         // Create JPEGS from data
         for (int i = 0; i < sizeof(buffer; i++))
         {
-            // if buffer i to i+2 is equal to the given values, and if the intersection of buffer[i+3] and 1111 0000 is 1110 0000 (meaning buffer[i+3] must have first four bits of 1110)
             if (buffer[i] == 0xff && buffer[i + 1] == 0xd8 && buffer[i + 2] == 0xff && (buffer[i + 3] & 0xf0) == 0xe0)
             {
                 sprintf(jpgname,"%03i.jpg",image_count);
