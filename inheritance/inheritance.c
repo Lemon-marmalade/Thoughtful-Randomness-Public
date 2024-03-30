d2e086f2ae1792d6e874c888e1d5a74b2f644352
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-30 03:59:31 +0000

    Fri, Mar 29, 2024, 8:59 PM -07:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -39,7 +39,7 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-    person *new_person = malloc(sizeof(person));
+    person *child = malloc(sizeof(person));
     // If there are still generations left to create
     if (generations > 1)
     {
@@ -48,8 +48,10 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-
+        child->parents[0] = parent0;
+        child->parents[1] = parent1;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
+        child->alleles[0] = parent0->alleles[rand() % 2]
     }
 
     // If there are no generations left to create
