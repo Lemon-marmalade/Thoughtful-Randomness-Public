3c8ece003e39522c3facdd1fab7a3ddfd2b5a06a
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-03-31 22:16:38 +0000

    Sun, Mar 31, 2024, 3:16 PM -07:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -58,7 +58,7 @@ bool load(const char *dictionary)
         // hash the word to obtain hash value
         int index = hash(word);
         // insert the new node into the hash table according to its hash value
-        
+        new->next = 
     }
     // add each word read. to the hash table
     // close dictionary file
