adffb8ef3e4a20c0f8f1b2cd6d0e1269ef5621bb
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-04-01 16:49:26 +0000

    Mon, Apr 1, 2024, 9:49 AM -07:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -39,7 +39,7 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    for (int i = 0; i < strlen(word))
+    for (int i = 0; i < strlen(word);)
     //
     return toupper(word[0]) - 'A';
 }
