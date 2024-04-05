4840a46bb8ce52352cb9b0e37f8295312630274d
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-04-05 00:40:50 +0000

    Thu, Apr 4, 2024, 5:40 PM -07:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -40,14 +40,14 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    // this hash function will initialize the hashvalue to the numerical value of the first letter
-    int hash_value = toupper(word[0]);
+    // this hash function will initialize the hashvalue to the numerical value of the first letter (0-25)
+    int hash_value = toupper(word[0]) - 'A';
     // if the word has more than 1 letter, it will multiply the previous hash value by 31 (an odd prime number in order to create more possibilities) before adding the next numerical value
     if (strlen(word) > 2)
     {
         for (int i = 1; i < strlen(word); i++)
         {
-            hash_value = hash_value * 31 + toupper(word[i]);
+            hash_value = hash_value * 31 + (toupper(word[i])-'A');
         }
     }
 
