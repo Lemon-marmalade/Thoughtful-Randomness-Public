ce193dfdfb207e440e2c4bd1365240c121449c16
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-04-06 01:33:03 +0000

    Fri, Apr 5, 2024, 6:33 PM -07:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -43,15 +43,7 @@ unsigned int hash(const char *word)
     // this hash function will initialize the hashvalue to the numerical value of the first letter (0-25)
     int hash_value = toupper(word[0]) - 'A';
     // if the word has more than 1 letter, it will multiply the previous hash value by 31 (an odd prime number in order to create more possibilities) before adding the next numerical value
-    int word_length = strlen(word);
-    if (word_length > 2)
-    {
-        for (int i = 1; i < word_length; i++)
-        {
-            hash_value = hash_value * 31 + (toupper(word[i])-'A');
-        }
-    }
-
+    return hash_value;
 }
 
 int wordcount = 0;
