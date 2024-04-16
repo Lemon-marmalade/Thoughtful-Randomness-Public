7a19d1e5ec06b642da7cd81cf8e9fde0ffa4cc5b
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-04-16 01:22:29 +0000

    Mon, Apr 15, 2024, 6:22 PM -07:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,9 +1,11 @@
 from cs50 import get_int
 
-while True:
-    number = get_int("Number: ")
-    if number >= 1:
-        break
+def main()
+    while True:
+        number = get_int("Number: ")
+        if number >= 1:
+            break
+        
 
 def calculate_digit(number)
     digit = 0
