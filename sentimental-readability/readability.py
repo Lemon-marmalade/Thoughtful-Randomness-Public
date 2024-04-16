a1ceeed59c8499ea0af78574c9adf444841a5fbf
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-04-16 23:45:25 +0000

    Tue, Apr 16, 2024, 4:45 PM -07:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -33,7 +33,7 @@ def calculate_words(text):
 def calculate_sentences(text):
     sentences = 0
     for i in range(len(text)):
-        if text[i] == "." or text[i] == "!" or text[i] == "?"
+        if text[i] == "." or text[i] == "!" or text[i] == "?":
             sentences += 1
     return sentences
 
