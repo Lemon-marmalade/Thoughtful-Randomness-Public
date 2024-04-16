8319c269703f8b518356ba613d4d791f622c5467
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-04-16 23:23:01 +0000

    Tue, Apr 16, 2024, 4:23 PM -07:00

---
+++ sentimental-readability/readability.py
@@ -0,0 +1,38 @@
+
+def main():
+    text = input("Text: ")
+
+    letters = calculate_letters(text)
+    words = calculate_words(text)
+    sentences = calculate_sentences(text)
+
+    index = 0.0588 * (letters * 100 / words) - 0.296 * (sentences * 100 / words) - 15.8
+
+    if index < 1:
+        print("Before Grade 1");
+
+    elif index > 16:
+        print("Grade 16+")
+    else:
+        printf(f"Grade {round(index)}")
+
+def calculate_letters(text):
+    letters = 0
+    for i in range(len(text)):
+        if text[i].isalpha():
+            letters += 1
+    return letters
+
+def calculate_words(text):
+    int words = 1
+    for i in range(len(text)):
+        if text[i].isspace():
+            words += 1
+    return words
+
+def calculate_sentences(text)
+    sentences = 0
+    for i in range(len(text)):
+        if text[i] == 46 or text[i] == 63 or text[i] == 33:
+            sentences += 1
+    return sentences
