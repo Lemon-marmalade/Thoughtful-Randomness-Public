38cd8e7b0a0d064c825bfc479d09fe71dbad337f
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-04-16 01:21:03 +0000

    Mon, Apr 15, 2024, 6:21 PM -07:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -5,6 +5,14 @@ while True:
     if number >= 1:
         break
 
-def calculate_digit()
-    
+def calculate_digit(number)
+    digit = 0
+    while number > 0:
+        digit = digit + number % 10
+        number = number / 10
+
+        digit = digit + (((number % 10) * 2) % 10 + ((number % 10) * 2) / 10)
+        number = number / 10
+    return digit
+
 
