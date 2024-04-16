b93861cf43b2c6fbe1e6a640bef274a9355b0626
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-04-16 03:18:00 +0000

    Mon, Apr 15, 2024, 8:18 PM -07:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,13 +1,14 @@
 from cs50 import get_int
 
+
 def main():
-    #infinitely, ask for number until given positive
+    # infinitely, ask for number until given positive
     while True:
         number = get_int("Number: ")
         if number >= 1:
             break
     digit = calculate_digit(number)
-    #check given conditions of each type
+    # check given conditions of each type
     if digit % 10 == 0:
         if number in range(340000000000000, 350000000000000) or number in range(370000000000000, 380000000000000):
             print("AMEX")
@@ -28,14 +29,15 @@ def main():
 def calculate_digit(number):
     digit = 0
     while number > 0:
-        #let digit be the last digit of the number
+        # let digit be the last digit of the number
         digit += number % 10
-        #move on
+        # move on
         number = (number / 10) - (number % 10) / 10
-        #add to digit, the next digit multiplied by 2's digits
-        digit += ((number % 10) * 2) % 10 + ((number % 10) * 2) / 10 - ((number % 10 * 2) % 10)/ 10
-        #move on
+        # add to digit, the next digit multiplied by 2's digits
+        digit += ((number % 10) * 2) % 10 + ((number % 10) * 2) / 10 - ((number % 10 * 2) % 10) / 10
+        # move on
         number = (number / 10) - (number % 10) / 10
     return digit
+
 
 main()
