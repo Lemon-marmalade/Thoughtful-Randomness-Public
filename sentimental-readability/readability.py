38688967aecba9d631896592c467859f10fa7095
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-04-16 23:23:38 +0000

    Tue, Apr 16, 2024, 4:23 PM -07:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -9,7 +9,7 @@ def main():
     index = 0.0588 * (letters * 100 / words) - 0.296 * (sentences * 100 / words) - 15.8
 
     if index < 1:
-        print("Before Grade 1");
+        print("Before Grade 1")
 
     elif index > 16:
         print("Grade 16+")
