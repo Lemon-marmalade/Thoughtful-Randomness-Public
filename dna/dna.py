67631a7ebe4f7a103df0f1e8e3c764978dc10e00
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-04-17 23:32:22 +0000

    Wed, Apr 17, 2024, 4:32 PM -07:00

--- dna/dna.py
+++ dna/dna.py
@@ -12,7 +12,8 @@ def main():
     # TODO: Read database file into a variable
             with open("sysargv[1]") as file:
                 reader = csv.DictReader(file)
-                csv.reader(sys.argv[1], )
+                for row in reader:
+                    rows.append(row)
     # TODO: Read DNA sequence file into a variable
 
     # TODO: Find longest match of each STR in DNA sequence
