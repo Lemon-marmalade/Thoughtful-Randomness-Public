5f6af33654af5c25749d44671bdaffe27fab9390
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-04-19 03:36:46 +0000

    Thu, Apr 18, 2024, 8:36 PM -07:00

--- dna/dna.py
+++ dna/dna.py
@@ -19,9 +19,7 @@ def main():
             with open(sys.argv[2], 'r') as file:
                 dna = file.read
     # TODO: Find longest match of each STR in DNA sequence
-            for i in range(len(database[1])-1):
-                longest_match(dna, database[i])
-
+                str = list(database[0].keys())[1:]
     # TODO: Check database for matching profiles
 
             return
