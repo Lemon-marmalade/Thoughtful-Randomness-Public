ffccfe29c6ccfe6245b2ba8cc5b3770a57f385af
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-04-19 01:29:24 +0000

    Thu, Apr 18, 2024, 6:29 PM -07:00

--- dna/dna.py
+++ dna/dna.py
@@ -16,10 +16,10 @@ def main():
                 for row in reader:
                     rows.append(row)
     # TODO: Read DNA sequence file into a variable
-            str = []
+            dna = []
             with open("sysargv[2]") as file:
                 reader = csv.DictReader(file)
-                rows.append(str)
+                rows.append(dna)
     # TODO: Find longest match of each STR in DNA sequence
 
     # TODO: Check database for matching profiles
