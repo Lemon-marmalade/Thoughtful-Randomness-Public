6f01c749ef3b011aabfdbc811cda995bd8b94a56
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-04-27 21:00:19 +0000

    Sat, Apr 27, 2024, 2:00 PM -07:00

--- songs/6.sql
+++ songs/6.sql
@@ -1,3 +1,3 @@
 SELECT name
 FROM songs
-WHERE songs.artist_id = (SELECT id FROM artists WHERE artists.name = 'Post Malone')
+WHERE songs.artist_id = (SELECT id FROM artists WHERE artists.name = 'Post Malone');
