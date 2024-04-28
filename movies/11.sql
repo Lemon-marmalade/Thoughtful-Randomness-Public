1938a9d502ca3a7518ba95e64cf938f146f11d92
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-04-28 18:05:09 +0000

    Sun, Apr 28, 2024, 11:05 AM -07:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,6 +1,6 @@
 SELECT title FROM movies
 WHERE id IN
 (SELECT movie_id FROM stars WHERE stars.person_id = (SELECT id FROM people WHERE people.name = 'Chadwick Boseman'))
-JOIN 
-ORDER BY ratings.rating DESC
+JOIN ratings ON ratings.movie_id = movies.id
+ORDER BY rating DESC
 LIMIT 5;
