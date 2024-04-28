da649b0af7c7eb0b3dea4e3a27a78229171fb58f
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-04-28 17:53:50 +0000

    Sun, Apr 28, 2024, 10:53 AM -07:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,4 +1,4 @@
 SELECT DISTINCT name FROM people
 WHERE id IN
-(SELECT person_id FROM stars WHERE stars.movie_id = (SELECT id FROM movies WHERE movies.year = 2004))
+(SELECT person_id FROM stars WHERE stars.movie_id IN (SELECT id FROM movies WHERE movies.year = 2004))
 ORDER BY people.birth;
