29d77e4314246afa48f8c4a785b3790369eed971
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-04-28 17:42:28 +0000

    Sun, Apr 28, 2024, 10:42 AM -07:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,2 +1,3 @@
 SELECT name FROM people
-WHERE people.id = (SELECT person_id FROM stars WHERE stars.movie_id = (SELECT id FROM movies WHERE movies.title = 'Toy Story'));
+WHERE id IN
+(SELECT person_id FROM stars WHERE stars.movie_id = (SELECT id FROM movies WHERE movies.title = 'Toy Story'));
