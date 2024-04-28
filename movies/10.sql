12d7fcd2e10c094d92798fd0f0a1ed4ac5e68e75
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-04-28 17:55:58 +0000

    Sun, Apr 28, 2024, 10:55 AM -07:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,4 +1,5 @@
 SELECT DISTINCT name FROM people
 WHERE id IN
 (SELECT person_id FROM directors WHERE directors.movie_id IN
-(SELECT id FROM movies WHERE movies.id = (SELECT movie_id FROM ratings WHERE rating >= 9.0)));
+(SELECT id FROM movies WHERE movies.id IN
+(SELECT movie_id FROM ratings WHERE rating >= 9.0)));
