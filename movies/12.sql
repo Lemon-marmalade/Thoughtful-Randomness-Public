c15ff82162484bad0c8e2dccdd89c20d06150a3a
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-04-28 18:38:57 +0000

    Sun, Apr 28, 2024, 11:38 AM -07:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,4 @@
 SELECT title FROM movies
 WHERE id IN
 (SELECT movie_id FROM stars WHERE stars.person_id IN
-(SELECT id FROM people WHERE people.name = 'Jennifer Lawrence') AND (SELECT id FROM people WHERE people.name = 'Bradley Cooper'));
+((SELECT id FROM people WHERE people.name = 'Jennifer Lawrence') AND (SELECT id FROM people WHERE people.name = 'Bradley Cooper')));
