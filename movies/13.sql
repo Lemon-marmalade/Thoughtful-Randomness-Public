e8c4da0e586cc574cd331b91182e0bbed985f940
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-04-28 19:26:59 +0000

    Sun, Apr 28, 2024, 12:26 PM -07:00

---
+++ movies/13.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT name FROM people
+WHERE id IN
+(SELECT person_id FROM stars WHERE stars.movie_id IN
+(SELECT id FROM movies WHERE movies.id IN
+(SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = '1958'))));
