6eedbd0d6c3fd1dc1f3b46a8119e8a1cf7f0f5a9
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-04-28 17:54:51 +0000

    Sun, Apr 28, 2024, 10:54 AM -07:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,3 +1,4 @@
 SELECT DISTINCT name FROM people
 WHERE id IN
-(SELECT person_id FROM directors WHERE directors.movie_id = (SELECT id FROM movies WHERE movies.id = (SELECT movie_id FROM ratings WHERE rating >= 9.0)));
+(SELECT person_id FROM directors WHERE directors.movie_id IN
+(SELECT id FROM movies WHERE movies.id = (SELECT movie_id FROM ratings WHERE rating >= 9.0)));
--- movies/9.sql
+++ movies/9.sql
@@ -1,4 +1,5 @@
 SELECT DISTINCT name FROM people
 WHERE id IN
-(SELECT person_id FROM stars WHERE stars.movie_id IN (SELECT id FROM movies WHERE movies.year = 2004))
+(SELECT person_id FROM stars WHERE stars.movie_id IN
+(SELECT id FROM movies WHERE movies.year = 2004))
 ORDER BY people.birth;
