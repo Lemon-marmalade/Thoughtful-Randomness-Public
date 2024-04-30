7762f95597c9465a8d904ba5d28d90f58a8eb72d
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-04-30 23:54:00 +0000

    Tue, Apr 30, 2024, 4:54 PM -07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -4,6 +4,8 @@ SELECT id, description FROM crime_scene_reports
     WHERE year = '2023' AND month = '7' AND day = '28' AND street = 'Humphrey Street';
 -- results:
 --description for the id of 295 is the only one that matches the crime
---the time was at 10:15am
+--the time of crime was at 10:15am
 --interviews were conducted on the day of the crime with 3 witnesses (each of their interview transcripts mentions the bakery)
--- 2:
+-- 2: selecting id, name and transcript from interviews on that same day (since information from previous query provided interviews were done the day of)
+SELECT id, name, transcript FROM interviews
+    WHERE year = '2023' AND month = '7' AND day = '28';
