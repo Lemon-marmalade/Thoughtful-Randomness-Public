0ef949d2abbc4f4d56ff3b69a66e028600ab40f1
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-05-03 02:46:17 +0000

    Thu, May 2, 2024, 7:46 PM -07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -60,9 +60,9 @@ SELECT name FROM people
         AND month = '7'
         AND day = '28'
         AND duration < 60))
-    AND people.license_plate IN (SELECT license_plate FROM bakery_security_logs
+    AND (people.license_plate IN (SELECT license_plate FROM bakery_security_logs
         WHERE year = '2023'
         AND month = '7'
         AND day = '28'
         AND hour = '10'
-        AND (minute BETWEEN 15 AND 25));
+        AND (minute BETWEEN 15 AND 25)));
