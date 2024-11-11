4e5a381e2ad756496d28d50a94c5e56134d60bcf
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-11-11 22:40:30 +0000

    Mon, Nov 11, 2024, 2:40 PM -08:00

---
+++ final/app.module.ts
@@ -0,0 +1,10 @@
+import { NgSelectModule } from '@ng-select/ng-select';
+
+@NgModule({
+  imports: [
+    NgSelectModule,
+    // other imports
+  ],
+  // other properties
+})
+export class AppModule { }
