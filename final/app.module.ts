4a6a1673110d502bb34baa99e178df9fa7d5e4a8
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-11-11 23:03:36 +0000

    Mon, Nov 11, 2024, 3:03 PM -08:00

--- final/app.module.ts
+++ final/app.module.ts
@@ -1,8 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
 import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
- @NgModule({
-  imports: [NgSelectModule, ...]
+  declarations: [
+    AppComponent,
+    // other components
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    NgSelectModule,
+    // other modules
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class YourModule { }
+export class AppModule { }
