45ccc816730d49c3a706434ab2847b5d844f3b97
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-11-11 16:26:18 +0000

    Mon, Nov 11, 2024, 8:26 AM -08:00

---
+++ final/app.py
@@ -0,0 +1,29 @@
+import os
+
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+    # Obtain number of people user wishes to have grouped
+
+    return render_template("index.html")
+
+@app.route("/input")
+def index():
+    # Have a 2 column table for user to fill names into and select preferences with
+
+    return render_template("input.html")
+
+@app.route("/groupings")
+def index():
+    # Display groupings in table
+
+    return render_template("groupings.html")
