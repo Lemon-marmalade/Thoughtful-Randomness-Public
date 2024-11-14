2d581113d1e943362c5f05937b604663f78b0a4f
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-11-14 15:45:29 +0000

    Thu, Nov 14, 2024, 7:45 AM -08:00

--- final/app.py
+++ final/app.py
@@ -50,7 +50,7 @@ def input():
         # Save info to session
         session['names'] = names
         session['preferences'] = preferences
-        # Do some group creating magic
+        # Do some group creating magic in create_groups :)
         groups = create_groups(names, preferences, num_groups)
 
         return render_template("groupings.html", groups=groups)
@@ -59,24 +59,25 @@ def input():
         return redirect("/")
 
 def create_groups(names, preferences, num_groups):
+    # Create empty groups for number of groups requests
     groups = [[] for _ in range(num_groups)]
+    # Shuffle names for randomness
     names = random.shuffle(names)
+
     preferences_associated = []
     free_names = []
     for name in names:
         # If the person has a preference
         if len(preferences[name]) > 0:
             preferences_associated.append(name)
-        # If not, they're free to be randomized
-        else:
-            free_names.append(name)
+        
         for other_name in names:
             # If they are a preference of someone else, append name too
             if name in preferences[other_name]:
                 preferences_associated.append(name)
         # Deal with those that are associated with preferences
-        sorted_names = sorted(sorted_names, key=lambda name: len(preferences[name]), reverse=True)
-    for name in sorted_names:
+        preferences_associated = sorted(preferences_associated, key=lambda name: len(preferences[name]), reverse=True)
+    for name in preferences_associated:
         best_group = find_best(name, groups, preferences[name], preferences)
         best_group.append(name)
     return groups
