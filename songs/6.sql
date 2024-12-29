28c1da78b6eeb9c87457bd020dddf09dc3f440e9
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-12-28 20:27:08 -0800

    Delete songs directory

--- songs/6.sql
+++
@@ -1,2 +0,0 @@
-SELECT name FROM songs
-WHERE songs.artist_id = (SELECT id FROM artists WHERE artists.name = 'Post Malone');
--- songs/7.sql
+++
@@ -1,3 +0,0 @@
-SELECT AVG(energy) FROM songs
-WHERE songs.artist_id = (SELECT id FROM artists WHERE artists.name = 'Drake');
-
