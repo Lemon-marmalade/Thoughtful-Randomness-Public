2771003a1ac476fdfe57592a0815c0fd6fcb3504
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2024-12-28 20:26:53 -0800

    Delete sentimental-mario-more directory

--- sentimental-mario-more/mario.py
+++
@@ -1,9 +0,0 @@
-from cs50 import get_int
-
-while True:
-    height = get_int("Height: ")
-    if height >= 1 and height <= 8:
-        break
-
-for i in range(height):
-    print(" " * (height - i - 1) + "#" * (i + 1) + "  " + "#" * (i + 1))
